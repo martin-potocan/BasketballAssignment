030b217d8f01c5644945f03ef954f4888f3ca954
Martin Potocan <martinpotocan1@gmail.com>
2025-12-04 19:39:53 +0100

    Initial commit

--- src/test/java/AverageThreePointersTest.java
+++ src/test/java/AverageThreePointersTest.java
@@ -1,5 +1,3 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
